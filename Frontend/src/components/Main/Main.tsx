8c0e66a5f3fad18428b5cc64f6a19fc018f57379
LublyFrench <aleshckevich.ilja@gmail.com>
2026-03-26 02:15:07 +0300

    Upgrade : css, backend

---
+++ Frontend/src/components/Main/Main.tsx
@@ -0,0 +1,17 @@
+import WelcomeFrame from "../UI/AuthFrames/WelcomeFrame"
+import SignInFrame from "../UI/AuthFrames/SignInFrame"
+import RegistrationFrame from "../UI/AuthFrames/RegistrationFrame"
+import LoginPage from "../../pages/AuthPage"
+
+function Main(){
+    return(
+        <main>
+            <h1>Main</h1>
+            <WelcomeFrame />
+            <SignInFrame />
+            <RegistrationFrame />
+            <LoginPage />
+        </main>
+    )
+}
+export default Main
